d56d044b7a7dcb07fb81a5a1489921528eda4381
Ted Mielczarek <ted@mielczarek.org>
2024-10-22 10:26:39 -0400

    add support for pre-playlist videos with src=

--- background.js
+++ background.js
@@ -1,21 +1,61 @@
-const URL_REGEX = new RegExp("https://files\.redgifs\.com/[^.]+\.m4s$", "m");
+const URL_REGEX = new RegExp("https://files.redgifs.com/[^.]+.(m4s|mp4)$", "m");
+
+function checkVideoURL() {
+  const player = document.querySelector(".Player-Video > video");
+  if (player && player.hasAttribute("src")) {
+    const src = player.getAttribute("src");
+    if (src) {
+      // We'll check if it matches the regex in the main script.
+      return src;
+    }
+  }
+  return null;
+}
+
+function downloadVideo(videoUrl) {
+  const filename = videoUrl.split("/").pop();
+  const gifNameCaps = filename.split(".")[0];
+  console.log(`downloading ${gifNameCaps} from ${videoUrl}`);
+  chrome.downloads.download({ url: videoUrl, filename: `${gifNameCaps}.mp4` });
+}
+
+async function findAndDownloadVideo(tab) {
+  // First, see if the video player has a `src` attribute (this is the easy case)
+  let injectionResults = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: checkVideoURL,
+  });
+  for (const { frameId, result } of injectionResults) {
+    // If we found it via <video src=""> then we're done!
+    //console.log(`checkVideoURL returned: '${result}'`);
+    if (result && URL_REGEX.test(result)) {
+      downloadVideo(result);
+      return;
+    }
+  }
+
+  // Otherwise, fetch the playlist and get the video URL from it.
+  const u = new URL(tab.url);
+  const gifname = u.pathname.split("/")[2];
+  if (gifname) {
+    const playlist = `https://api.redgifs.com/v2/gifs/${gifname}/sd.m3u8`;
+    console.log(`fetching playlist: ${playlist}`);
+    let response = await fetch(playlist);
+    let text = await response.text();
+    const matches = URL_REGEX.exec(text);
+    if (matches) {
+      const videoUrl = matches[0];
+      downloadVideo(videoUrl);
+    } else {
+      console.error(
+        `Didn't find any video matches. Playlist contents:\n${text}`
+      );
+    }
+  } else {
+    console.error(`Couldn't determine gif name for URL: ${tab.url}`);
+  }
+}
 
 chrome.action.onClicked.addListener((tab) => {
-    const u = new URL(tab.url);
-    const gifname = u.pathname.split("/")[2];
-    if (gifname) {
-        const playlist = `https://api.redgifs.com/v2/gifs/${gifname}/sd.m3u8`;
-        console.log(`fetching playlist: ${playlist}`);
-        fetch(playlist).then((response) => response.text())
-        .then((text) => {
-            const matches = URL_REGEX.exec(text);
-            if (matches) {
-                const videoUrl = matches[0];
-                const filename = videoUrl.split("/").pop();
-                const gifNameCaps = filename.split(".")[0];
-                console.log(`downloading ${gifNameCaps} from ${videoUrl}`);
-                chrome.downloads.download({url: videoUrl, filename: `${gifNameCaps}.mp4`});
-            }
-        });
-    }
+  findAndDownloadVideo(tab);
 });
